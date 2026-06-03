4885786bf72baba11753d28f46cfd1dd97ab10d6
A.J. Rodriguez <ajrodriguez@AJs-MacBook-Pro.local>
2026-06-03 14:21:32 -0400

    Title change
    - Title change.

--- src/app/ui/advocates/advocates-header.tsx
+++ src/app/ui/advocates/advocates-header.tsx
@@ -2,7 +2,7 @@ export default function AdvocatesHeader() {
   return (
     <div>
       <div className="w-full bg-gradient-to-r from-emerald-900 to-white p-4 rounded-md">
-        <h1 className="text-3xl font-bold text-white">Solace Advocates</h1>
+        <h1 className="text-3xl font-bold text-white">Health Advocates</h1>
       </div>
     </div>
   );
--- src/app/ui/advocates/advocates-search-section.tsx
+++ src/app/ui/advocates/advocates-search-section.tsx
@@ -12,7 +12,7 @@ export default function AdvocatesSearchSection({
         <Card className="w-2/3 border-gray-300 drop-shadow-lg">
           <CardHeader>
             <CardTitle className="text-2xl font-bold">Search Advocates</CardTitle>
-            <CardDescription>Find the Solace health advocate who is right for you!</CardDescription>
+            <CardDescription>Find the health advocate who is right for you!</CardDescription>
           </CardHeader>
           <CardContent>
             <AdvocatesSearchInput searchInputText={searchInputText}/>
